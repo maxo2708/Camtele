b14eae0a4bbf888700b8d46f836e28f3c2d2b558
Brandon Rozek <hello@brandonrozek.com>
2017-04-23 22:27:17 -0400

    Added tags

--- src/Tag.java
+++ src/Tag.java
@@ -1,4 +1,4 @@
-public abstract class Tag {
+public class Tag implements Comparable {
     Posts postings;
     String name;
     public Tag(String name) {
@@ -7,5 +7,23 @@ public abstract class Tag {
     }
     public Posts getPosts() {
         return this.postings;
+    }
+    public String getName() { return this.name; }
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Tag t2 = (Tag) obj;
+        return this.getName().equals(t2.getName());
+    }
+    public int compareTo(Object obj) {
+        Tag t2 = (Tag) obj;
+        return this.getName().compareTo(t2.getName());
     }
 }
---
+++ src/Tags.java
@@ -0,0 +1,57 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Tags implements Listing<Tag>, Serializable {
+    ArrayList<Tag> tags;
+
+    public Tags() {
+        this.tags = new ArrayList<Tag>();
+    }
+
+    public boolean add(Tag t) {
+        boolean addedSuccessfully = this.tags.add(t);
+        Collections.sort(this.tags);
+        return addedSuccessfully;
+    }
+
+    public Tag remove(int index) {
+        return this.tags.remove(index);
+    }
+
+    public Tag get(int index) {
+        return this.tags.get(index);
+    }
+
+    public int size() { return this.tags.size(); }
+
+    public String toString() {
+        String returns = "";
+        for (Tag t: this.tags) {
+            returns += t.toString();
+        }
+        return returns;
+    }
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Tags tags2 = (Tags) obj;
+        if (this.tags.size() != tags2.size()) {
+            return false;
+        }
+        // Assumption: Structure is sorted before comparing indexes
+        for (int i = 0; i < this.tags.size(); i++) {
+            if (!this.tags.get(i).equals(tags2.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
